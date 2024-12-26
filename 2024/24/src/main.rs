a4fcde5d297f3a300bdf58266010cba5d6569430
juhotuho10 <juhotuho10@gmail.com>
2024-12-26 19:13:28 +0200

    2024 day 24 complete

--- 2024/24/src/main.rs
+++ 2024/24/src/main.rs
@@ -15,12 +15,13 @@ and we have to find the gates that have been swapped and give them as output alp
 
 */
 
-use fxhash::FxHashMap;
+use fxhash::{FxHashMap, FxHashSet};
+use itertools::Itertools;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::time::Instant;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 #[allow(clippy::upper_case_acronyms)]
 enum Gate {
     AND,
@@ -28,7 +29,7 @@ enum Gate {
     XOR,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 struct Instruction {
     num1: String,
     num2: String,
@@ -40,7 +41,7 @@ struct Instruction {
 struct AllInstructions {
     values: FxHashMap<String, bool>,
     instructions: Vec<Instruction>,
-    sus_instruction: Vec<Instruction>,
+    sus_instruction: FxHashSet<Instruction>,
     ins_num_1: Vec<String>,
     ins_num_2: Vec<String>,
     ins_target: Vec<String>,
@@ -108,7 +109,7 @@ impl AllInstructions {
         AllInstructions {
             values: start_values,
             instructions: all_instructions,
-            sus_instruction: vec![],
+            sus_instruction: FxHashSet::default(),
             ins_num_1,
             ins_num_2,
             ins_target,
@@ -150,6 +151,14 @@ impl AllInstructions {
     }
 
     fn values_to_num(&self, start_char: char) -> u64 {
+        let bool_values = self.values_to_bool(start_char);
+
+        bool_values
+            .iter()
+            .fold(0, |acc, bit| (acc << 1) | *bit as u64)
+    }
+
+    fn values_to_bool(&self, start_char: char) -> Vec<bool> {
         let mut copy_values = self.values.clone();
 
         copy_values.retain(|s, _| s.starts_with(start_char));
@@ -159,10 +168,10 @@ impl AllInstructions {
         key_value_pairs.sort_by(|a, b| a.0.cmp(b.0));
 
         key_value_pairs
-            .iter()
+            .into_iter()
             .rev()
-            .map(|(_, val)| **val)
-            .fold(0, |acc, bit| (acc << 1) | bit as u64)
+            .map(|(_, val)| *val)
+            .collect()
     }
 
     fn xyz_nums(&self) -> (u64, u64, u64) {
@@ -188,7 +197,143 @@ impl AllInstructions {
         (xyz_nums[0], xyz_nums[1], xyz_nums[2])
     }
 
-    fn get_sus_instructions(&mut self) {}
+    fn get_sus_instructions(&mut self) {
+        let max_num = self.values.len() / 2;
+        let mut working_instructions = vec![];
+
+        for current_num in 0..max_num {
+            for iter_num in 0..=2 {
+                let mut copy_self = self.clone();
+
+                let mut bools = vec![false; max_num + 1];
+
+                copy_self.values.values_mut().for_each(|v| *v = false);
+
+                let key_str_y = format!("y{:02}", current_num);
+                let key_str_x = format!("x{:02}", current_num);
+
+                match iter_num {
+                    0 => {
+                        copy_self.values.insert(key_str_x, true);
+
+                        bools[current_num] = true;
+                    }
+                    1 => {
+                        copy_self.values.insert(key_str_y, true);
+
+                        bools[current_num] = true;
+                    }
+                    2 => {
+                        copy_self.values.insert(key_str_x, true);
+
+                        copy_self.values.insert(key_str_y, true);
+
+                        bools[current_num + 1] = true;
+                    }
+                    _ => unreachable!(),
+                }
+
+                let rev_bools: Vec<bool> = bools.into_iter().rev().collect();
+
+                let mut used_instructions = vec![];
+
+                let mut available_instructions: Vec<Instruction> = vec![];
+                loop {
+                    copy_self.instructions.retain(|ins| {
+                        let all_values: Vec<&String> = copy_self.values.keys().collect();
+
+                        if all_values.contains(&&ins.num1) && all_values.contains(&&ins.num2) {
+                            available_instructions.push(ins.clone());
+                            false
+                        } else {
+                            true
+                        }
+                    });
+
+                    if available_instructions.is_empty() {
+                        break;
+                    }
+
+                    for curr_ins in &available_instructions {
+                        let val_1 = copy_self.values[&curr_ins.num1];
+                        let val_2 = copy_self.values[&curr_ins.num2];
+
+                        if val_1 || val_2 {
+                            used_instructions.push(curr_ins.clone());
+                        }
+
+                        let result = match curr_ins.gate {
+                            Gate::AND => val_1 & val_2,
+                            Gate::OR => val_1 | val_2,
+                            Gate::XOR => val_1 ^ val_2,
+                        };
+
+                        copy_self.values.insert(curr_ins.target.clone(), result);
+                    }
+
+                    available_instructions.clear();
+
+                    if copy_self.instructions.is_empty() {
+                        break;
+                    }
+                }
+
+                if copy_self.values_to_bool('z') == rev_bools {
+                    working_instructions.extend(used_instructions.clone());
+                } else {
+                    self.sus_instruction.extend(used_instructions.clone());
+                }
+            }
+        }
+
+        self.sus_instruction
+            .retain(|ins| !working_instructions.contains(ins));
+
+        dbg!(&self.sus_instruction);
+
+        dbg!(&self.sus_instruction.len());
+    }
+
+    fn find_wrong_instruction(&self) -> Vec<Instruction> {
+        let mut self_copy = self.clone();
+
+        self_copy
+            .instructions
+            .retain(|ins| !self_copy.sus_instruction.contains(ins));
+
+        let original_swap_wires = 8;
+        let manual_confirmed: Vec<Instruction> = self_copy
+            .sus_instruction
+            .clone()
+            .into_iter()
+            .filter(|ins| ["z10", "z32", "z39", "grm", "twr", "ggn"].contains(&ins.target.as_str()))
+            .collect();
+
+        self_copy
+            .sus_instruction
+            .retain(|ins| !manual_confirmed.contains(ins));
+
+        let unsure_swaps = original_swap_wires - manual_confirmed.len();
+
+        let mut counter = 0;
+        for mut combination in self
+            .sus_instruction
+            .clone()
+            .into_iter()
+            .combinations(unsure_swaps)
+        {
+            combination.extend(manual_confirmed.clone());
+
+            for permutation in combination.into_iter().permutations(original_swap_wires) {
+                dbg!(permutation);
+                counter += 1;
+            }
+        }
+
+        dbg!(counter);
+
+        vec![]
+    }
 }
 
 fn main() {
@@ -214,14 +359,8 @@ fn part_1(_my_input: &[String]) {
 }
 
 fn part_2(_my_input: &[String]) {
-    let example_2 = read_file("example_2.txt");
-    dbg!(&example_2);
-
-    let example_sum = solution_2(&example_2);
-    dbg!(&example_sum);
-    assert_eq!(example_sum, 0);
-
     let my_sum = solution_2(_my_input);
+    assert_eq!(my_sum, "ggn,grm,jcb,ndw,twr,z10,z32,z39");
     dbg!(my_sum);
 }
 
@@ -234,9 +373,12 @@ fn run_circuit_1(input: &[String]) -> u64 {
     all_instructions.values_to_num('z')
 }
 
-fn solution_2(input: &[String]) -> u32 {
-    let all_instructions = AllInstructions::from_string(input);
-    0
+fn solution_2(input: &[String]) -> String {
+    let mut all_instructions = AllInstructions::from_string(input);
+
+    all_instructions.get_sus_instructions();
+    //all_instructions.find_wrong_instruction();
+    "".to_owned()
 }
 
 fn read_file(file_name: &str) -> Vec<String> {
