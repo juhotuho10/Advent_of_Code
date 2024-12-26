67ab18e36a0211866aa9c076f37236699f45a600
juhotuho10 <juhotuho10@gmail.com>
2024-12-27 01:48:26 +0200

    2023 day 18 uncompleted second attempt

--- 2023/18/src/main.rs
+++ 2023/18/src/main.rs
@@ -6,11 +6,16 @@ part 2:
 the instructions were messed up and we need to parse the hex code given as the wall color to be the the direction and color
 */
 
-use core::panic;
 use regex::Regex;
 use std::collections::{HashMap, VecDeque};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum Turn {
+    Left,
+    Right,
+}
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Dir {
@@ -18,6 +23,12 @@ enum Dir {
     R,
     D,
     L,
+}
+
+#[derive(Debug, Clone)]
+struct Corner {
+    dir: Dir,
+    corner: Coord,
 }
 #[derive(Debug, Clone)]
 struct Instruction {
@@ -67,21 +78,13 @@ impl Instruction {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, PartialOrd, Ord, Copy)]
 struct Coord {
     x: i64,
     y: i64,
 }
 
 impl Coord {
-    fn go_dir(&self, dir: Dir) -> Self {
-        match dir {
-            Dir::U => self.up(),
-            Dir::R => self.right(),
-            Dir::D => self.down(),
-            Dir::L => self.left(),
-        }
-    }
     fn up(&self) -> Self {
         Coord {
             x: self.x,
@@ -109,132 +112,96 @@ impl Coord {
             y: self.y,
         }
     }
+
+    fn go_dir(&self, dir: Dir) -> Self {
+        match dir {
+            Dir::U => self.up(),
+            Dir::R => self.right(),
+            Dir::D => self.down(),
+            Dir::L => self.left(),
+        }
+    }
 }
 
-#[derive(Debug, Clone)]
-struct TrenchPart {
-    dug: bool,
-    wall_part: bool,
-    outside: Option<bool>,
+struct CornerTracer {
+    prev_dir: Option<Dir>,
+    current_pos: Coord,
+    outside_pos: Coord,
+    pos_visited: Vec<Coord>,
 }
 
-#[derive(Debug, Clone)]
-struct Trench {
-    instructions: Vec<Instruction>,
-    trench_map: HashMap<Coord, TrenchPart>,
-}
+impl CornerTracer {
+    fn new() -> Self {
+        let default_coord = Coord { x: 0, y: 0 };
+        CornerTracer {
+            prev_dir: None,
+            current_pos: default_coord,
+            outside_pos: default_coord,
+            pos_visited: vec![default_coord],
+        }
+    }
 
-impl Trench {
-    fn from_instructions(instructions: Vec<Instruction>) -> Self {
-        let mut trench_map: HashMap<Coord, TrenchPart> = HashMap::new();
-        let mut current_coord = Coord { x: 0, y: 0 };
-        let starting_hole = TrenchPart {
-            dug: true,
-            wall_part: true,
-            outside: Some(false),
+    fn go_to_corner(&mut self, inst: Instruction) {
+        // 0,0 - 0,0 |   -> R | None
+        // 0,7 - 1,0 | R -> D | Right
+        // 6,7 - 1,1 | D -> L | Right
+        // 6,5 - 1,1 | L -> D | Left
+        // 7,5 - 1,0 | D -> R | Left
+        // 7,7 - 1,0 | R -> D | Right
+        // 10,7- 1,1 | D -> L | Right
+
+        let turn: Option<Turn> = match (self.prev_dir, inst.dir) {
+            (None, _) => None,
+            (Some(Dir::U), Dir::L)
+            | (Some(Dir::L), Dir::D)
+            | (Some(Dir::D), Dir::R)
+            | (Some(Dir::R), Dir::U) => Some(Turn::Left),
+            (Some(Dir::U), Dir::R)
+            | (Some(Dir::R), Dir::D)
+            | (Some(Dir::D), Dir::L)
+            | (Some(Dir::L), Dir::U) => Some(Turn::Right),
+            (_, _) => unreachable!(),
         };
 
-        trench_map.insert(current_coord.clone(), starting_hole);
+        self.prev_dir = Some(inst.dir);
 
-        for inst in &instructions {
-            for _ in 0..inst.count {
-                current_coord = current_coord.go_dir(inst.dir);
+        match inst.dir {
+            Dir::U => self.current_pos.y -= inst.count as i64,
 
-                let starting_hole = TrenchPart {
-                    dug: true,
-                    wall_part: true,
-                    outside: Some(false),
-                };
+            Dir::R => self.current_pos.x += inst.count as i64,
 
-                trench_map.insert(current_coord.clone(), starting_hole);
-            }
+            Dir::D => self.current_pos.y += inst.count as i64,
+
+            Dir::L => self.current_pos.x -= inst.count as i64,
         }
 
-        let min_x = trench_map.keys().map(|coord| coord.x).min().unwrap();
-        let min_y = trench_map.keys().map(|coord| coord.y).min().unwrap();
+        self.outside_pos = match turn {
+            None => self.outside_pos,
+            Some(Turn::Left) => match self.outside_pos {
+                Coord { x: 0, y: 0 } => Coord { x: 0, y: 1 },
+                Coord { x: 0, y: 1 } => Coord { x: 1, y: 1 },
+                Coord { x: 1, y: 1 } => Coord { x: 1, y: 0 },
+                Coord { x: 1, y: 0 } => Coord { x: 1, y: 0 }, // exception
+                _ => unreachable!(),
+            },
+            Some(Turn::Right) => match self.outside_pos {
+                Coord { x: 0, y: 0 } => Coord { x: 1, y: 0 },
+                Coord { x: 1, y: 0 } => Coord { x: 1, y: 1 },
+                Coord { x: 1, y: 1 } => Coord { x: 0, y: 1 },
+                Coord { x: 0, y: 1 } => Coord { x: 0, y: 1 }, // exception
 
-        let max_x = trench_map.keys().map(|coord| coord.x).max().unwrap();
-        let max_y = trench_map.keys().map(|coord| coord.y).max().unwrap();
+                _ => unreachable!(),
+            },
+        };
 
-        // add outside ring for easier way to distinguish outside and inside parts
-        for y in (min_y - 1)..=(max_y + 1) {
-            for x in (min_x - 1)..=(max_x + 1) {
-                let coord = Coord { x, y };
+        let last_visited = self.pos_visited.last_mut().unwrap();
+        last_visited.x += self.outside_pos.x;
+        last_visited.y += self.outside_pos.y;
 
-                trench_map.entry(coord).or_insert_with(|| TrenchPart {
-                    dug: false,
-                    wall_part: false,
-                    outside: None,
-                });
-            }
-        }
+        //dbg!(self.outside_pos);
+        dbg!(turn);
 
-        Trench {
-            instructions,
-            trench_map,
-        }
-    }
-
-    fn mark_outside(&mut self) {
-        let start_coord = self.trench_map.keys().min().unwrap().to_owned();
-        {
-            let start_hole = self.trench_map.get_mut(&start_coord).unwrap();
-            start_hole.outside = Some(true);
-        }
-
-        let mut checking_coords: VecDeque<Coord> = VecDeque::new();
-        checking_coords.push_back(start_coord);
-
-        let possible_dirs = [Dir::U, Dir::D, Dir::L, Dir::R];
-        while let Some(coord) = checking_coords.pop_front() {
-            for dir in possible_dirs {
-                let dir_coord = coord.go_dir(dir);
-                if let Some(dir_location) = self.trench_map.get_mut(&dir_coord) {
-                    if dir_location.outside.is_none() {
-                        dir_location.outside = Some(true);
-                        checking_coords.push_back(dir_coord);
-                    }
-                }
-            }
-        }
-    }
-
-    fn dig_hole(&mut self) {
-        for part in self.trench_map.values_mut() {
-            if part.outside.is_none() {
-                part.outside = Some(false);
-                part.dug = true;
-            }
-        }
-    }
-
-    fn get_inside_count(&self) -> u64 {
-        self.trench_map
-            .values()
-            .filter(|part| part.outside == Some(false))
-            .count() as u64
-    }
-
-    fn print_trench(&self) {
-        let min_x = self.trench_map.keys().map(|coord| coord.x).min().unwrap();
-        let min_y = self.trench_map.keys().map(|coord| coord.y).min().unwrap();
-
-        let max_x = self.trench_map.keys().map(|coord| coord.x).max().unwrap();
-        let max_y = self.trench_map.keys().map(|coord| coord.y).max().unwrap();
-
-        for y in min_y..=max_y {
-            let mut line_string = "".to_owned();
-            for x in min_x..=max_x {
-                let coord = Coord { x, y };
-                let current = self.trench_map.get(&coord).unwrap();
-                match current.dug {
-                    true => line_string += "#",
-                    false => line_string += ".",
-                }
-            }
-
-            println!("{}", &line_string);
-        }
+        self.pos_visited.push(self.current_pos);
     }
 }
 
@@ -255,8 +222,6 @@ fn part_1(_my_input: &[String]) {
 
     let my_volume = get_trench_volume_1(_my_input);
     dbg!(my_volume);
-
-    panic!();
 }
 
 fn part_2(_my_input: &[String]) {
@@ -271,60 +236,49 @@ fn part_2(_my_input: &[String]) {
     //dbg!(my_sum);
 }
 
-fn get_trench_volume_1(input: &[String]) -> u128 {
-    //let mut trench = parse_input_1(input);
-    //
-    ////trench.print_trench();
-    //trench.mark_outside();
-    //trench.dig_hole();
-    ////trench.print_trench();
-    //trench.get_inside_count()
+fn get_trench_volume_1(input: &[String]) -> u64 {
+    let instructions = parse_input_1(input);
 
-    let test_corners = [
-        (0, 0),
-        (0, 4),
-        (2, 4),
-        (2, 2),
-        (3, 2),
-        (3, 4),
-        (4, 4),
-        (4, 0),
-        (0, 0),
-    ];
+    dbg!(&instructions);
 
-    dbg!(area_from_corners(&test_corners));
-    panic!();
+    //dbg!(parse_input_to_corners(instructions));
 
-    let corners = parse_input_to_corners_1(input);
-    dbg!(&corners);
-    calculate_buffered_area(&corners, 0.45)
+    let mut corner_tracer = CornerTracer::new();
+
+    for ins in instructions {
+        corner_tracer.go_to_corner(ins);
+    }
+
+    dbg!(corner_tracer.pos_visited);
+
+    0
 }
 
 fn get_trench_volume_2(input: &[String]) -> u64 {
-    let mut trench = parse_input_2(input);
+    let instructions = parse_input_2(input);
 
-    trench.mark_outside();
-    trench.dig_hole();
-    trench.get_inside_count()
+    dbg!(parse_input_to_corners(instructions));
+
+    0
 }
 
-fn parse_input_1(input: &[String]) -> Trench {
-    let instructions: Vec<Instruction> = input
+fn parse_input_1(input: &[String]) -> Vec<Instruction> {
+    input
         .iter()
         .map(|s| Instruction::from_string_1(s))
-        .collect();
-
-    Trench::from_instructions(instructions)
+        .collect()
 }
 
-fn parse_input_to_corners_1(input: &[String]) -> Vec<(i64, i64)> {
-    let instructions: Vec<Instruction> = input
+fn parse_input_2(input: &[String]) -> Vec<Instruction> {
+    input
         .iter()
-        .map(|s| Instruction::from_string_1(s))
-        .collect();
+        .map(|s| Instruction::from_string_2(s))
+        .collect()
+}
 
+fn parse_input_to_corners(instructions: Vec<Instruction>) -> Vec<Corner> {
     let mut current_coords = (0, 0);
-    let mut coords: Vec<(i64, i64)> = vec![current_coords];
+    let mut coords: Vec<Corner> = vec![];
 
     for inst in &instructions {
         match inst.dir {
@@ -337,13 +291,21 @@ fn parse_input_to_corners_1(input: &[String]) -> Vec<(i64, i64)> {
             Dir::L => current_coords.0 -= inst.count as i64,
         }
 
-        coords.push(current_coords);
+        let new_corner = Corner {
+            dir: inst.dir,
+            corner: Coord {
+                x: current_coords.0,
+                y: current_coords.1,
+            },
+        };
+
+        coords.push(new_corner);
     }
 
     coords
 }
 
-fn area_from_corners(corners: &[(i64, i64)]) -> u128 {
+fn area_from_corners(corners: &[(i64, i64)]) -> u64 {
     let mut sum1 = 0;
     let mut sum2 = 0;
 
@@ -355,41 +317,7 @@ fn area_from_corners(corners: &[(i64, i64)]) -> u128 {
         sum2 += coords_1.1 * coords_2.0;
     }
 
-    (sum1.abs_diff(sum2) as f64 / 2.0) as u128
-}
-
-fn calculate_buffered_area(corners: &[(i64, i64)], thickness: f64) -> u128 {
-    let interior_area = area_from_corners(corners) as f64; // Shoelace Formula for interior area
-    let mut edge_buffer_area = 0.0;
-    let mut corner_buffer_area = 0.0;
-
-    let n = corners.len();
-
-    for i in 0..n - 1 {
-        let (x1, y1) = corners[i];
-        let (x2, y2) = corners[i + 1];
-
-        // Length of the edge
-        let edge_length = (((x2 - x1).pow(2) + (y2 - y1).pow(2)) as f64).sqrt();
-
-        // Buffer area for the edge
-        edge_buffer_area += edge_length * thickness;
-    }
-
-    // Buffer area for the corners (approximate as squares)
-    corner_buffer_area = n as f64 * (thickness * thickness);
-
-    // Total area
-    (interior_area + edge_buffer_area + corner_buffer_area) as u128
-}
-
-fn parse_input_2(input: &[String]) -> Trench {
-    let instructions: Vec<Instruction> = input
-        .iter()
-        .map(|s| Instruction::from_string_2(s))
-        .collect();
-
-    Trench::from_instructions(instructions)
+    (sum1.abs_diff(sum2) as f64 / 2.0) as u64
 }
 
 fn read_file(file_name: &str) -> Vec<String> {
