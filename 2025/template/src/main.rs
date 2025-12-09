d9b4d4521f52b44c8e305bc6deb67512b8ec1f28
juhotuho10 <juhotuho10@gmail.com>
2025-12-09 19:19:05 +0200

    update template

--- 2025/template/src/main.rs
+++ 2025/template/src/main.rs
@@ -5,8 +5,11 @@ part 2:
 
 */
 
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    time::Instant,
+};
 
 fn main() {
     let my_input = read_file("my_input.txt");
@@ -19,24 +22,30 @@ fn part_1(_my_input: &[String]) {
     let example_1 = read_file("example_1.txt");
     dbg!(&example_1);
 
-    let example_sum = solution_1(&example_1);
-    dbg!(&example_sum);
-    assert_eq!(example_sum, 0);
+    let example_1 = solution_1(&example_1);
+    dbg!(&example_1);
+    assert_eq!(example_1, 0);
 
-    let my_sum = solution_1(_my_input);
-    dbg!(my_sum);
+    let start = Instant::now();
+    let solution_1 = solution_1(_my_input);
+    let elapsed = start.elapsed().as_micros();
+    println!("Time to solve problem 1: {elapsed}µs");
+    dbg!(solution_1);
 }
 
 fn part_2(_my_input: &[String]) {
     let example_2 = read_file("example_2.txt");
     dbg!(&example_2);
 
-    let example_sum = solution_2(&example_2);
-    dbg!(&example_sum);
-    assert_eq!(example_sum, 0);
+    let example_2 = solution_2(&example_2);
+    dbg!(&example_2);
+    assert_eq!(example_2, 0);
 
-    let my_sum = solution_2(_my_input);
-    dbg!(my_sum);
+    let start = Instant::now();
+    let solution_2 = solution_2(_my_input);
+    let elapsed = start.elapsed().as_micros();
+    println!("Time to solve problem 2: {elapsed}µs");
+    dbg!(solution_2);
 }
 
 fn solution_1(input: &[String]) -> u32 {
